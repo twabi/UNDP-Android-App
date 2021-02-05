45ee68d138ec39e2b8e3a9902788c728866c785c
kenpachi <itwabi@gmail.com>
2021-02-05 12:19:28 +0200

    added a drawer layout in the userHome layout

--- app/src/main/java/com/example/wastemgmtapp/LogInActivity.java
+++ app/src/main/java/com/example/wastemgmtapp/LogInActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LogInActivity extends AppCompatActivity {
 
     TextView noAccountText;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,20 @@ public class LogInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_log_in);
 
         noAccountText = findViewById(R.id.noAccount);
+        login = findViewById(R.id.btn_login);
 
         noAccountText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LogInActivity.this, UserSignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogInActivity.this, UserHomeActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/wastemgmtapp/UserHomeActivity.java
@@ -0,0 +1,46 @@
+package com.example.wastemgmtapp;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class UserHomeActivity extends AppCompatActivity{
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle mToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home);
+
+        toolbar = findViewById(R.id.nav_action);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        setSupportActionBar(toolbar);
+
+        mToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

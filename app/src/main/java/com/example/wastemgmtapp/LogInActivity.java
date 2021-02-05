281ffdeb1750ea90ebe50e7930649cc9736386c2
kenpachi <itwabi@gmail.com>
2021-02-05 09:29:37 +0200

    Implemented Switching between login and signup screens

--- app/src/main/java/com/example/wastemgmtapp/LogInActivity.java
+++ app/src/main/java/com/example/wastemgmtapp/LogInActivity.java
@@ -2,13 +2,28 @@ package com.example.wastemgmtapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LogInActivity extends AppCompatActivity {
+
+    TextView noAccountText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        noAccountText = findViewById(R.id.noAccount);
+
+        noAccountText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogInActivity.this, UserSignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/wastemgmtapp/UserSignUpActivity.java
+++ app/src/main/java/com/example/wastemgmtapp/UserSignUpActivity.java
@@ -2,13 +2,26 @@ package com.example.wastemgmtapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class UserSignUpActivity extends AppCompatActivity {
 
+    TextView alreadyAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_up);
+
+        alreadyAccount = findViewById(R.id.already);
+        alreadyAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserSignUpActivity.this, LogInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

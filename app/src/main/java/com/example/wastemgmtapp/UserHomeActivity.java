9f6aeef15b822fc7495d83a071ceedf43e5ba891
kenpachi <itwabi@gmail.com>
2021-02-06 11:03:13 +0200

    added the mapbox api to the project

--- app/src/main/java/com/example/wastemgmtapp/UserHomeActivity.java
+++ app/src/main/java/com/example/wastemgmtapp/UserHomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.wastemgmtapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,15 +10,23 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 
 public class UserHomeActivity extends AppCompatActivity{
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle mToggle;
+    private MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_user_home);
 
         toolbar = findViewById(R.id.nav_action);
@@ -31,6 +40,25 @@ public class UserHomeActivity extends AppCompatActivity{
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+
+
+                    }
+                });
+
+            }
+        });
 
     }
 

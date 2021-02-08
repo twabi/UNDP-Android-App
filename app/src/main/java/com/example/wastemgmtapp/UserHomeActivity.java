9d1f92943cd5612d7a7ce045dd4440b56b4f6e75
kenpachi <itwabi@gmail.com>
2021-02-08 09:13:40 +0200

    added the user home layout and some map code.

--- app/src/main/java/com/example/wastemgmtapp/UserHomeActivity.java
+++ app/src/main/java/com/example/wastemgmtapp/UserHomeActivity.java
@@ -9,8 +9,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.google.android.material.navigation.NavigationView;
+import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
@@ -43,6 +46,10 @@ public class UserHomeActivity extends AppCompatActivity{
 
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+
+        CameraPosition position = new CameraPosition.Builder()
+                .target(new LatLng(-15.786111, 35.005833)).zoom(10).tilt(20)
+                .build();
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull MapboxMap mapboxMap) {
@@ -50,16 +57,14 @@ public class UserHomeActivity extends AppCompatActivity{
                 mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
-
                         // Map is set up and the style has loaded. Now you can add data or make other map adjustments
-
+                        mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 10);
 
                     }
                 });
 
             }
         });
-
     }
 
     @Override
@@ -70,5 +75,47 @@ public class UserHomeActivity extends AppCompatActivity{
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }

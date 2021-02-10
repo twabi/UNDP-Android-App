33f0c35ec61b9a317857ba56b21d3fbd54abd1cd
kenpachi <itwabi@gmail.com>
2021-02-10 14:27:53 +0200

    more codacy cleanUp

--- app/src/main/java/com/example/wastemgmtapp/TrashRecyclerAdapter.java
+++ app/src/main/java/com/example/wastemgmtapp/TrashRecyclerAdapter.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 

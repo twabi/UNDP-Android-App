81e14164298ed5951fccf776ac26d0a74cfaadc0
twabi <itwabi@gmail.com>
2021-03-29 12:43:07 +0200

    fetching all three task types

--- app/src/main/java/com/example/wastemgmtapp/Staff/CollectionRequests.java
+++ app/src/main/java/com/example/wastemgmtapp/Staff/CollectionRequests.java
@@ -21,6 +21,8 @@ import com.apollographql.apollo.exception.ApolloException;
 import com.apollographql.apollo.subscription.SubscriptionTransport;
 import com.apollographql.apollo.subscription.WebSocketSubscriptionTransport;
 import com.example.wastemgmtapp.Common.SessionManager;
+import com.example.wastemgmtapp.GetTaskSortedWastesQuery;
+import com.example.wastemgmtapp.GetTaskTrashCollectionsQuery;
 import com.example.wastemgmtapp.GetTasksQuery;
 import com.example.wastemgmtapp.R;
 import com.example.wastemgmtapp.TaskAddedSubscription;
@@ -48,6 +50,7 @@ public class CollectionRequests extends AppCompatActivity {
     RecyclerView tasksView;
     LinearLayout errorLayout, noItems;
     ProgressBar loadTasks;
+    ArrayList<Object> tasks = new ArrayList<>();
     ApolloClient apolloClient;
     RequestsRecyclerAdapter recyclerAdapter;
     @Override
@@ -78,17 +81,24 @@ public class CollectionRequests extends AppCompatActivity {
                 .build();
         apolloClient = ApolloClient.builder().okHttpClient(httpClient)
                 .serverUrl("https://waste-mgmt-api.herokuapp.com/graphql")
-                .subscriptionTransportFactory(
-                       new WebSocketSubscriptionTransport.Factory("wss://waste-mgmt-api.herokuapp.com/graphql", httpClient))
                 .build();
 
         apolloClient.query(new GetTasksQuery()).enqueue(taskCallback());
-        apolloClient.subscribe(new TaskAddedSubscription()).execute(subscriptionCallback());
+        apolloClient.query(new GetTaskSortedWastesQuery()).enqueue(taskSortedCallback());
+        apolloClient.query(new GetTaskTrashCollectionsQuery()).enqueue(taskCollectCallback());
 
         errorLayout.setOnClickListener(view -> {
             clearDS();
             apolloClient.query(new GetTasksQuery()).enqueue(taskCallback());
         });
+
+        if(keyList.size() == 0){
+            loadTasks.setVisibility(View.VISIBLE);
+            noItems.setVisibility(View.VISIBLE);
+        } else {
+            errorLayout.setVisibility(View.GONE);
+            noItems.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -135,7 +145,6 @@ public class CollectionRequests extends AppCompatActivity {
                         Log.d(TAG, "tasks fetched: " + data.tasks());
 
                         try{
-                            ArrayList<Object> tasks = new ArrayList<>();
                             if(!TextUtils.isEmpty(userID)){
                                 for(int i=0; i < data.tasks().size(); i++){
                                     if((userID.equals(data.tasks().get(i).staff()._id()) && (data.tasks().get(i).completed() == false))){
@@ -143,25 +152,7 @@ public class CollectionRequests extends AppCompatActivity {
                                         keyList.add(data.tasks().get(i)._id());
                                         statusList.add(data.tasks().get(i).completed());
                                         createdAtList.add(data.tasks().get(i).createdAt());
-
-                                        /*
-
-                                        Log.d(TAG, "requests: " + data.tasks().get(i).sortedWaste() + "-" + data.tasks().get(i).trashcollection());
-
-                                        if(data.tasks().get(i).sortedWaste() != null && data.tasks().get(i).trashcollection() == null){
-                                            taskType.add("Sorted Waste Collection");
-                                        } else if(data.tasks().get(i).trashcollection() != null && data.tasks().get(i).sortedWaste() == null){
-                                            taskType.add("Trash Collection");
-                                        } else if(data.tasks().get(i).trashcollection() == null && data.tasks().get(i).sortedWaste() == null){
-                                            Log.d(TAG, "The body: " + data.tasks().get(i).body());
-                                            if(data.tasks().get(i).body().equals("not available")){
-                                                taskType.add("Unknown Task");
-                                            } else {
-                                                taskType.add("Other");
-                                            }
-                                        }
-
-                                         */
+                                        taskType.add("General");
                                     }
                                 }
 
@@ -208,126 +199,189 @@ public class CollectionRequests extends AppCompatActivity {
     }
 
 
-    public ApolloSubscriptionCall.Callback<TaskAddedSubscription.Data> subscriptionCallback(){
-        return new ApolloSubscriptionCall.Callback<TaskAddedSubscription.Data>() {
-
+    public ApolloCall.Callback<GetTaskSortedWastesQuery.Data> taskSortedCallback(){
+        return new ApolloCall.Callback<GetTaskSortedWastesQuery.Data>() {
             @Override
-            public void onResponse(@NotNull Response response) {
-                TaskAddedSubscription.Data data = (TaskAddedSubscription.Data) response.getData();
-                Log.d(TAG, "onResponse: " + data);
+            public void onResponse(@NotNull Response<GetTaskSortedWastesQuery.Data> response) {
+                GetTaskSortedWastesQuery.Data data = response.getData();
 
-                try{
-                    if(data.getClass() == null){
 
-                        if(response.getErrors() == null){
-                            Log.e(TAG, "an unknown Error in tasks query : " );
-                            runOnUiThread(() -> {
-                                loadTasks.setVisibility(View.GONE);
-                                Toast.makeText(CollectionRequests.this,
-                                        "an unknown Error occurred : " , Toast.LENGTH_LONG).show();
+                if(data.taskSortedWastes() == null){
 
-                            });
-                        } else{
-                            List<Error> error = response.getErrors();
-                            String errorMessage = error.get(0).getMessage();
-                            Log.e(TAG, "an Error in tasks query : " + errorMessage );
-                            runOnUiThread(() -> {
-                                loadTasks.setVisibility(View.GONE);
-                                Toast.makeText(CollectionRequests.this,
-                                        "an Error occurred : " + errorMessage, Toast.LENGTH_LONG).show();
-
-                            });
-                        }
-                    }else{
+                    if(response.getErrors() == null){
+                        Log.e(TAG, "an unknown Error in tasks query : " );
                         runOnUiThread(() -> {
-                        /*
+                            loadTasks.setVisibility(View.GONE);
+                            errorLayout.setVisibility(View.VISIBLE);
+                            Toast.makeText(CollectionRequests.this,
+                                    "an unknown Error occurred : " , Toast.LENGTH_LONG).show();
+
+                        });
+                    } else{
+                        List<Error> error = response.getErrors();
+                        String errorMessage = error.get(0).getMessage();
+                        Log.e(TAG, "an Error in tasks query : " + errorMessage );
+                        runOnUiThread(() -> {
+                            loadTasks.setVisibility(View.GONE);
+                            errorLayout.setVisibility(View.VISIBLE);
+                            Toast.makeText(CollectionRequests.this,
+                                    "an Error occurred : " + errorMessage, Toast.LENGTH_LONG).show();
+
+                        });
+                    }
+                }else{
+                    runOnUiThread(() -> {
                         loadTasks.setVisibility(View.GONE);
                         noItems.setVisibility(View.GONE);
                         errorLayout.setVisibility(View.GONE);
-                        Log.d(TAG, "tasks fetched: " + data.tasks());
-                        ArrayList<Object> tasks = new ArrayList<>();
-                        if(!TextUtils.isEmpty(userID)){
-                            for(int i=0; i < data.tasks().size(); i++){
-                                if(userID.equals(data.tasks().get(i).staff()._id())){
-                                    tasks.add(data.tasks().get(i));
-                                    keyList.add(data.tasks().get(i)._id());
-                                    statusList.add(data.tasks().get(i).completed());
-                                    createdAtList.add(data.tasks().get(i).createdAt());
+                        Log.d(TAG, "tasks fetched: " + data.taskSortedWastes());
 
-                                    if(data.tasks().get(i).sortedWaste() != null && data.tasks().get(i).trashcollection() == null){
-                                        taskType.add("Sorted Waste Collection");
-                                    } else if(data.tasks().get(i).trashcollection() != null && data.tasks().get(i).sortedWaste() == null){
-                                        taskType.add("Trash Collection");
-                                    } else if(data.tasks().get(i).trashcollection() == null && data.tasks().get(i).sortedWaste() == null){
-                                        Log.d(TAG, "The body: " + data.tasks().get(i).body());
-                                        if(data.tasks().get(i).body().equals("not available")){
-                                            taskType.add("Unknown Task");
-                                        } else {
-                                            taskType.add("Other");
-                                        }
-
+                        try{
+                            if(!TextUtils.isEmpty(userID)){
+                                for(int i=0; i < data.taskSortedWastes().size(); i++){
+                                    if((userID.equals(data.taskSortedWastes().get(i).staff()._id()) && (data.taskSortedWastes().get(i).completed() == false))){
+                                        tasks.add(data.taskSortedWastes().get(i));
+                                        keyList.add(data.taskSortedWastes().get(i)._id());
+                                        statusList.add(data.taskSortedWastes().get(i).completed());
+                                        createdAtList.add(data.taskSortedWastes().get(i).createdAt());
+                                        taskType.add("SortedWaste");
                                     }
                                 }
-                            }
 
-                            recyclerAdapter = new RequestsRecyclerAdapter(CollectionRequests.this, keyList, statusList, createdAtList, taskType);
-                            tasksView.setAdapter(recyclerAdapter);
+                                recyclerAdapter = new RequestsRecyclerAdapter(CollectionRequests.this, keyList, statusList, createdAtList, taskType);
+                                tasksView.setAdapter(recyclerAdapter);
 
-                            if(recyclerAdapter.getItemCount() == 0){
-                                noItems.setVisibility(View.VISIBLE);
+                                if(recyclerAdapter.getItemCount() == 0){
+                                    noItems.setVisibility(View.VISIBLE);
+                                }
                             }
+                        }catch (Exception e){
+                            errorLayout.setVisibility(View.VISIBLE);
+                            Toast.makeText(CollectionRequests.this,
+                                    "an Error occurred : " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
 
-                            }
-                         */
+                    });
+
+                    if(response.getErrors() != null){
+                        List<Error> error = response.getErrors();
+                        String errorMessage = error.get(0).getMessage();
+                        Log.e(TAG, "an Error in tasks query : " + errorMessage );
+                        runOnUiThread(() -> {
+                            Toast.makeText(CollectionRequests.this,
+                                    "an Error occurred : " + errorMessage, Toast.LENGTH_LONG).show();
 
                         });
-
-                        if(response.getErrors() != null){
-                            List<Error> error = response.getErrors();
-                            String errorMessage = error.get(0).getMessage();
-                            Log.e(TAG, "an Error in tasks query : " + errorMessage );
-                            runOnUiThread(() -> {
-                                Toast.makeText(CollectionRequests.this,
-                                        "an Error occurred : " + errorMessage, Toast.LENGTH_LONG).show();
-
-                            });
-                        }
                     }
-                }catch (Exception e){
-                    e.printStackTrace();
                 }
-
-
-
             }
 
             @Override
             public void onFailure(@NotNull ApolloException e) {
                 Log.e(TAG, "Error", e);
                 runOnUiThread(() -> {
+                    errorLayout.setVisibility(View.VISIBLE);
                     loadTasks.setVisibility(View.GONE);
                     Toast.makeText(CollectionRequests.this,
                             "An error occurred : " + e.getMessage(), Toast.LENGTH_LONG).show();
 
                 });
             }
+        };
+    }
 
+
+    public ApolloCall.Callback<GetTaskTrashCollectionsQuery.Data> taskCollectCallback(){
+        return new ApolloCall.Callback<GetTaskTrashCollectionsQuery.Data>() {
             @Override
-            public void onCompleted() {
+            public void onResponse(@NotNull Response<GetTaskTrashCollectionsQuery.Data> response) {
+                GetTaskTrashCollectionsQuery.Data data = response.getData();
 
+
+                if(data.taskTrashCollections() == null){
+
+                    if(response.getErrors() == null){
+                        Log.e(TAG, "an unknown Error in tasks query : " );
+                        runOnUiThread(() -> {
+                            loadTasks.setVisibility(View.GONE);
+                            errorLayout.setVisibility(View.VISIBLE);
+                            Toast.makeText(CollectionRequests.this,
+                                    "an unknown Error occurred : " , Toast.LENGTH_LONG).show();
+
+                        });
+                    } else{
+                        List<Error> error = response.getErrors();
+                        String errorMessage = error.get(0).getMessage();
+                        Log.e(TAG, "an Error in tasks query : " + errorMessage );
+                        runOnUiThread(() -> {
+                            loadTasks.setVisibility(View.GONE);
+                            errorLayout.setVisibility(View.VISIBLE);
+                            Toast.makeText(CollectionRequests.this,
+                                    "an Error occurred : " + errorMessage, Toast.LENGTH_LONG).show();
+
+                        });
+                    }
+                }else{
+                    runOnUiThread(() -> {
+                        loadTasks.setVisibility(View.GONE);
+                        noItems.setVisibility(View.GONE);
+                        errorLayout.setVisibility(View.GONE);
+                        Log.d(TAG, "tasks fetched: " + data.taskTrashCollections());
+
+                        try{
+                            if(!TextUtils.isEmpty(userID)){
+                                for(int i=0; i < data.taskTrashCollections().size(); i++){
+                                    if((userID.equals(data.taskTrashCollections().get(i).staff()._id()) && (data.taskTrashCollections().get(i).completed() == false))){
+                                        tasks.add(data.taskTrashCollections().get(i));
+                                        keyList.add(data.taskTrashCollections().get(i)._id());
+                                        statusList.add(data.taskTrashCollections().get(i).completed());
+                                        createdAtList.add(data.taskTrashCollections().get(i).createdAt());
+                                        taskType.add("TrashCollection");
+                                    }
+                                }
+
+                                recyclerAdapter = new RequestsRecyclerAdapter(CollectionRequests.this, keyList, statusList, createdAtList, taskType);
+                                tasksView.setAdapter(recyclerAdapter);
+
+                                if(recyclerAdapter.getItemCount() == 0){
+                                    noItems.setVisibility(View.VISIBLE);
+                                }
+                            }
+                        }catch (Exception e){
+                            errorLayout.setVisibility(View.VISIBLE);
+                            Toast.makeText(CollectionRequests.this,
+                                    "an Error occurred : " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+
+                    });
+
+                    if(response.getErrors() != null){
+                        List<Error> error = response.getErrors();
+                        String errorMessage = error.get(0).getMessage();
+                        Log.e(TAG, "an Error in tasks query : " + errorMessage );
+                        runOnUiThread(() -> {
+                            Toast.makeText(CollectionRequests.this,
+                                    "an Error occurred : " + errorMessage, Toast.LENGTH_LONG).show();
+
+                        });
+                    }
+                }
             }
 
             @Override
-            public void onTerminated() {
+            public void onFailure(@NotNull ApolloException e) {
+                Log.e(TAG, "Error", e);
+                runOnUiThread(() -> {
+                    errorLayout.setVisibility(View.VISIBLE);
+                    loadTasks.setVisibility(View.GONE);
+                    Toast.makeText(CollectionRequests.this,
+                            "An error occurred : " + e.getMessage(), Toast.LENGTH_LONG).show();
 
-            }
-
-            @Override
-            public void onConnected() {
-
+                });
             }
         };
     }
+
 
     public void clearDS(){
         taskType.clear();

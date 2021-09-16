fed27dd1aec4922f539327af1ee16908596066ec
twabi <itwabi@gmail.com>
2021-09-16 17:48:10 +0200

    minor bug fix

--- app/src/main/java/com/undp/wastemgmtapp/Fragments/CollectionFragment.java
+++ app/src/main/java/com/undp/wastemgmtapp/Fragments/CollectionFragment.java
@@ -94,9 +94,6 @@ public class CollectionFragment extends Fragment {
             @Override
             public void onResponse(@NotNull Response<GetCollectionNotifsQuery.Data> response) {
                 GetCollectionNotifsQuery.Data data = response.getData();
-
-
-
                     if(data.trashCollectionNotications() == null){
 
                         if(response.getErrors() == null){
@@ -143,7 +140,7 @@ public class CollectionFragment extends Fragment {
                                         }
 
                                     } else {
-                                        noItems.setVisibility(View.VISIBLE);
+                                        //noItems.setVisibility(View.VISIBLE);
                                     }
                                 }
 
